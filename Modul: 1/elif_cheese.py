e87c57765196cc81f6d349a9d9a1860a275849cf
Cram-In <bizior.marcin@googlemail.com>
2020-10-22 11:56:28 +0200

    Initial Commit

---
+++ Modul: 1/elif_cheese.py
@@ -0,0 +1,7 @@
+available_cheese = ""
+if available_cheese == "edam":
+    print("Buy edam")
+elif available_cheese == "brie":
+    print("Buy Brie")
+else:
+    print("Buy Gouda")
---
+++ Modul: 1/testy.py
@@ -0,0 +1,16 @@
+for a in range(6, 0, -1):
+    if a > 1:
+        print("*"*a)
+    elif a == 1:
+        print("*"*a)
+    else:
+        pass
+# Nr 2
+for i in range(2,7,1):
+    print("*"*2)
+for i in range(1,3):
+    print("*"*4)
+for i in range(1,3):
+    print("*"*6)
+for i in range(1,3):
+    print("*"*8)
