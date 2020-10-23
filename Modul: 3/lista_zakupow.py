988ad180b5ece97728568b5ad5e61f82062a5ba5
Cram-In <bizior.marcin@googlemail.com>
2020-10-23 11:47:43 +0200

    update

--- Modul: 3/lista_zakupow.py
+++ Modul: 3/lista_zakupow.py
@@ -1,1 +1,13 @@
-print("Lista zakupow")
+lista_zakupów = {"Piekarnia": ["Chleb", "bułki", "Pączek"], "Warzywniak": ["Marchew", "Seler", "Rokula"]}
+
+
+for shop, item in lista_zakupów.items():
+    print(f"Idę do {shop} i kupuję tam: {item}".format(shop,item))
+    
+    count = 0
+for shop, item in lista_zakupów.items():
+    if isinstance(item, list):
+        count += len(item)
+    print(f"W sumie kupuję {count} produktów.".format(count))
+
+    
