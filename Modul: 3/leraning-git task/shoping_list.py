c4cfae0b1f04b5c86b0f40cf34040f837c200711
Cram-In <bizior.marcin@googlemail.com>
2020-10-23 16:20:52 +0200

    upper case shops and start upper case for values

--- Modul: 3/leraning-git task/shoping_list.py
+++ Modul: 3/leraning-git task/shoping_list.py
@@ -1,9 +1,9 @@
-# your shopping list
+#your shopping list
 shoping_list = {"Piekarnia": ["Chleb", "bułki", "Pączek"], "Warzywniak": ["Marchew", "Seler", "Rokula"]}
-
-# print lists
+#uppercase list
+#upper_list = ((k.upper(), v.upper()) for k, v in shoping_list.items())
+#print lists
 for shop, item in shoping_list.items():
- 
     print(f"Idę do {shop.upper()} i kupuję tam: {item}")
 #count items on the list  
 count = 0
