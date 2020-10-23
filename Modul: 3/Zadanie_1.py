f090b6ca82bc194b19199e0c5fbb1d7fd9769c24
Cram-In <bizior.marcin@googlemail.com>
2020-10-23 13:47:44 +0200

    cleaning f-trings

--- Modul: 3/Zadanie_1.py
+++ Modul: 3/Zadanie_1.py
@@ -1,10 +1,10 @@
-lista_zakupów = {"Piekarnia": ["Chleb", "bułki", "Pączek"], "Warzywniak": ["Marchew", "Seler", "Rokula"]}
+shoping_list = {"Piekarnia": ["Chleb", "bułki", "Pączek"], "Warzywniak": ["Marchew", "Seler", "Rokula"]}
 
-for shop, item in lista_zakupów.items():
-    print(f"Idę do {shop} i kupuję tam: {item}".format(shop,item))
+for shop, item in shoping_list.items():
+    print(f"Idę do {shop} i kupuję tam: {item}")
     
 count = 0
-for shop, item in lista_zakupów.items():
+for shop, item in shoping_list.items():
     if isinstance(item, list):
         count += len(item)
-print(f"W sumie kupuję {count} produktów.".format(count))
+print(f"W sumie kupuję {count} produktów.")
