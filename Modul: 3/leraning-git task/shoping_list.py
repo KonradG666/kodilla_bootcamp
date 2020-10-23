174c6c1923e1f196154406a8fb3600ef814a66b7
Cram-In <bizior.marcin@googlemail.com>
2020-10-23 15:46:08 +0200

    shop in upper

--- Modul: 3/leraning-git task/shoping_list.py
+++ Modul: 3/leraning-git task/shoping_list.py
@@ -1,7 +1,9 @@
 # your shopping list
 shoping_list = {"Piekarnia": ["Chleb", "bułki", "Pączek"], "Warzywniak": ["Marchew", "Seler", "Rokula"]}
+
 # print lists
 for shop, item in shoping_list.items():
+ 
     print(f"Idę do {shop.upper()} i kupuję tam: {item}")
 #count items on the list  
 count = 0
