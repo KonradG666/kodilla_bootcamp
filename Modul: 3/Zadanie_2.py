40a759d76918ffc9cfe266f5375f05d08bf9cd34
Cram-In <bizior.marcin@googlemail.com>
2020-10-23 13:51:50 +0200

    cleaning

--- Modul: 3/Zadanie_2.py
+++ Modul: 3/Zadanie_2.py
@@ -6,5 +6,5 @@ for num in range(0,100):
     elif num % 3 == 0:
         by_3.append(num)
 
-print(f"Liczby podzielen przez 5: {by_5}".format(by_5))
-print(f"Liczby podzielne przez 3: {by_3}".format(by_3))
+print(f"Liczby podzielen przez 5: {by_5}")
+print(f"Liczby podzielne przez 3: {by_3}")
