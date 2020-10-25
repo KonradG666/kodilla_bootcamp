80518449ee5957aeb795bcaad0f1594de03b3b76
Cram-In <bizior.marcin@googlemail.com>
2020-10-25 15:11:33 +0100

    additional minor change

--- Modul: 3/leraning-git task/shoping_list.py
+++ Modul: 3/leraning-git task/shoping_list.py
@@ -1,8 +1,8 @@
 #your shopping list
-shoping_list = {"Piekarnia": ["Chleb", "bułki", "Pączek"], "Warzywniak": ["Marchew", "Seler", "Rokula"]}
+shoping_list = {"Piekarnia": ["Chleb", "Bułki", "Pączek"], "Warzywniak": ["Marchew", "Seler", "Rokula"]}
 #list to upper case
 upper_list = {k.upper():[i.upper() for i in v] for k,v in shoping_list.items()}
-#print lists
+#print shopping lists for each shop
 for shop, item in upper_list.items():
     print(f"Idę do {shop} i kupuję tam: {item}")
 #count items on the list  && print 
