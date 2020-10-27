9f7b8577424f92fd4139652d49f39b05417f0c10
Marcin <bizior.marcin@googlemail.com>
2020-10-27 09:41:08 +0100

    fix

--- Modul_3/Zadanie_1.py
+++ Modul_3/Zadanie_1.py
@@ -8,5 +8,4 @@ for shop, item in shoping_list.items():
     if isinstance(item, list):
         count += len(item)
 print(f"W sumie kupuję {count} produktów.")
-
 #print(f"W sumie kupuję {sum([len(shoping_list[x]) for x in shoping_list if isinstance(shoping_list[x], list)])} produktów.")
