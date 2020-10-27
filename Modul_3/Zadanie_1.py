07926c09de95515638ebd9aa846fec40eec9fae5
Marcin <bizior.marcin@googlemail.com>
2020-10-27 09:35:01 +0100

    second print option added

--- Modul_3/Zadanie_1.py
+++ Modul_3/Zadanie_1.py
@@ -8,3 +8,5 @@ for shop, item in shoping_list.items():
     if isinstance(item, list):
         count += len(item)
 print(f"W sumie kupuję {count} produktów.")
+
+print(f"W sumie kupuję {sum([len(shoping_list[x]) for x in shoping_list if isinstance(shoping_list[x], list)])} produktów.")
