0d239bd9c987f4580ebe423d542bc99731a255d1
Marcin <bizior.marcin@googlemail.com>
2020-10-27 09:38:38 +0100

    remove exrta line

--- Modul_3/Zadanie_1.py
+++ Modul_3/Zadanie_1.py
@@ -9,4 +9,4 @@ for shop, item in shoping_list.items():
         count += len(item)
 print(f"W sumie kupuję {count} produktów.")
 
-print(f"W sumie kupuję {sum([len(shoping_list[x]) for x in shoping_list if isinstance(shoping_list[x], list)])} produktów.")
+#print(f"W sumie kupuję {sum([len(shoping_list[x]) for x in shoping_list if isinstance(shoping_list[x], list)])} produktów.")
