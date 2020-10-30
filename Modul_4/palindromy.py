fdf39f7210bfb8f852353dce519b62979066d4c4
Marcin <bizior.marcin@googlemail.com>
2020-10-31 00:09:03 +0100

    adding txt examples

--- Modul_4/palindromy.py
+++ Modul_4/palindromy.py
@@ -7,3 +7,10 @@ def palindrome(txt):
     txt = "".join(l for l in txt if l.isalnum())
     # checking if string is a palindrome through slicing
     return txt == txt[::-1]
+
+palindrome("kajak")
+palindrome("kajak")
+palindrome("abba")
+palindrome("Are we not drawn onward, we few, drawn onward to new era?")
+#knock knock
+palindrome("tattarrattat")
