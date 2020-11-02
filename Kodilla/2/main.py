abcb28f03c8e6dc7da21e0fc3067946f53ca92b3
Marcin <bizior.marcin@googlemail.com>
2020-11-02 15:20:53 +0100

    updates

--- Kodilla/2/main.py
+++
@@ -1,3 +0,0 @@
-print("'Spanish Inquisition' to najlepszy skecz grupy Monty Python")
-print("----------------------------------------------------------------------------------")
-print("Python refactoring tool bicyclerepair was named after 'Bicycle Repair Man' sketch")
