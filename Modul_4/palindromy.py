e80ad237267c7538f952634e6418285ecb507a38
Marcin <bizior.marcin@googlemail.com>
2020-11-02 11:01:48 +0100

    completed

--- Modul_4/palindromy.py
+++ Modul_4/palindromy.py
@@ -7,10 +7,11 @@ def palindrome(txt):
     txt = "".join(l for l in txt if l.isalnum())
     # checking if string is a palindrome through slicing
     return txt == txt[::-1]
+    print(bool)
 
-palindrome("kajak")
-palindrome("kajak")
-palindrome("abba")
+#palindrome("kajak")
+#palindrome("kajak")
+#palindrome("abba")
 palindrome("Are we not drawn onward, we few, drawn onward to new era?")
 #knock knock
-palindrome("tattarrattat")
+#palindrome("tattarrattat")
