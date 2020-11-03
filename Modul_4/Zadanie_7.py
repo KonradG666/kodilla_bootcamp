0224af732560d8a820d77295c8d89f4c3b2bf8a9
Marcin <bizior.marcin@googlemail.com>
2020-11-03 11:59:58 +0100

    fix age limit

--- Modul_4/Zadanie_7.py
+++ Modul_4/Zadanie_7.py
@@ -4,18 +4,17 @@ Czy istnieje teraz możliwość zmiany minimalnego poziomu logowania? Sprawdź w
 """
 import sys
 import logging
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(message)s')
 
 def call_out(name, age):
-    if age > 18:
+    if age >= 18:
         print(f"Yo! {name} want a beer?")
     else:
         print(f"Hey {name} want some juice?")
 
 
 if __name__ == "__main__":
-    logging.debug("The program was called with this parameters %s" % sys.argv[1:])
-    logging.debug("First parameter is %s" % sys.argv[1])
+    logging.debug("The program was called with this parameters %s" % sys.argv[1])
     name = sys.argv[1]
     age = int(sys.argv[2])
     call_out(name, age)
