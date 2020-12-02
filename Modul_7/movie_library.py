6fbd795b1a9d07e58108db926d592d9b47d50e57
Marcin <bizior.marcin@googlemail.com>
2020-12-02 14:12:59 +0100

    fix

--- Modul_7/movie_library.py
+++ Modul_7/movie_library.py
@@ -7,8 +7,7 @@ class Movie():
         self.genre = genre
         self.watched = watched
         #vareable
-        self._watched = watched
-        
+        self._watched = watched        
 
     def __str__(self):
         return f"{self.title} {self.release}"
@@ -30,18 +29,18 @@ class TvSeries(Movie):
         return f"{self.title} {self.season}{self.episode}"
 
 main_library = [
-    {"Movie":"title": "Mad Max 2", release = "1981", genre = "action", watched = 0)},
-    {"Movie":"title": "Die Hard", release = "1988", genre = "action", watched = 0)},
-    {"Movie":"title": "Nausicaa of the Valley of the Wind", release = "1984", genre = "animation", watched = 0)},
-    {"Movie":"title": "Castle in the Sky", release = "1986", genre = "animation", watched = 0)},
-    {"Movie":"title": "Monty Python's Life of Brian", release = "1979", genre = "comedy", watched = 0)},
-    {"Movie":"title": "Blade Runner", release = "1982", genre = "syfy", watched = 0)},
-    {"TvSeries":"title": "The Soptanos", release = "1999", genre = "crime", episode = "E01", season = "S01", watched = 0)},
-    {"TvSeries":"title": "The Office", release = "2005", genre = "sitcom", episode = "E02", season = "S04", watched = 0)},
-    {"TvSeries":"title": "Doctor Who", release = "1963", genre = "syfy", episode = "E01", season = "S08", watched = 0)},
-    {"TvSeries":"title": "Battlestar Galactica", release = "2004", genre = "syfy", episode = "E04", season = "S02", watched = 0)},
-    {"TvSeries":"title": "South Park", release = "1997", genre = "animation", episode = "E05", season = "S14", watched = 0)},
-    {"TvSeries":"title": "Fawlty Towers", release = "1975", genre = "sitcom", episode = "E03", season = "S02", watched = 0)}
+    mov_1 = Movie(title = "Mad Max 2", release = "1981", genre = "action", watched = 0),
+    mov_2 = Movie(title = "Die Hard", release = "1988", genre = "action", watched = 0),
+    mov_3 = Movie(title = "Nausicaa of the Valley of the Wind", release = "1984", genre = "animation", watched = 0),
+    mov_4 = Movie(title = "Castle in the Sky", release = "1986", genre = "animation", watched = 0),
+    mov_5 = Movie(title = "Monty Python's Life of Brian", release = "1979", genre = "comedy", watched = 0),
+    mov_6 = Movie(title = "Blade Runner", release = "1982", genre = "syfy", watched = 0),
+    seri_1 = TvSeries(title = "The Soptanos", release = "1999", genre = "crime", episode = "E01", season = "S01", watched = 0),
+    seri_2 = TvSeries(title = "The Office", release = "2005", genre = "sitcom", episode = "E02", season = "S04", watched = 0),
+    seri_3 = TvSeries(title = "Doctor Who", release = "1963", genre = "syfy", episode = "E01", season = "S08", watched = 0),
+    seri_4 = TvSeries(title = "Battlestar Galactica", release = "2004", genre = "syfy", episode = "E04", season = "S02", watched = 0),
+    seri_5 = TvSeries(title = "South Park", release = "1997", genre = "animation", episode = "E05", season = "S14", watched = 0),
+    seri_6 = TvSeries(title = "Fawlty Towers", release = "1975", genre = "sitcom", episode = "E03", season = "S02", watched = 0)
     ]
 
 print(f"movie1 {mov_1.current_views}")
@@ -55,12 +54,10 @@ def get_movies():
     for i in main_library:
         if i == Movie:
             return i
-
 def get_series():
     for i in main_library:
         if i == TvSeries:
             return i
-
 def search():
     look_up = input("Type the title you are lookig for: ")
     for i in main_library:
@@ -101,5 +98,5 @@ def run():
             search()
         elif op == OP_TOP:
             print(top_titles)
-
+#start
 run()
