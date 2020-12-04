10aebfc4174418d5d486d255b11fe7821da03c8e
Marcin <bizior.marcin@googlemail.com>
2020-12-05 00:59:11 +0100

    update

--- Modul_7/movie_library/start.py
+++ Modul_7/movie_library/start.py
@@ -1,12 +1,14 @@
-import all_func
+import utils
 import main
+from utils import top
 
 def run():
     print("\t\tBibloteka filmów\n"+"\t\t"+"-"*16)
-    generate_views()
-    print(f"Najpopularniejsze filmy i seriale dnia {now}\n"+"\t\t"+"*"*10)
-    top_titles()
+    utils.generate_views()
+    print(f"Najpopularniejsze filmy i seriale z dnia {utils.now}\n"+"to:")
+    utils.top_titles()
+    utils.top_3(top)
     print("-"*60 + "\n \t\tMain Menu\n")
-    menu()
+    main.menu()
 
 run()
