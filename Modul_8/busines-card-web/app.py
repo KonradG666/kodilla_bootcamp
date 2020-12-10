d85e4211ac705123d0779d3debe3687bb9d32d49
Marcin <bizior.marcin@googlemail.com>
2020-12-10 12:27:50 +0100

    app updates

--- Modul_8/busines-card-web/app.py
+++ Modul_8/busines-card-web/app.py
@@ -1,12 +1,17 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
     return render_template("index.html")
+    
+@app.route('/index.html')
+def menu():
+    return render_template("index.html")
 
-@app.route('/about_me.html', methods=['GET'])
+@app.route('/about_me.html')
 def about_me():
     return render_template("about_me.html")
 
@@ -14,7 +19,4 @@ def about_me():
 def contact_me():
     return render_template("contact_me.html")
 
-@app.route('/index.html')
-def menu():
-    return render_template("index.html")
 
