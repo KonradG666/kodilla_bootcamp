8efbca4c9958f9c6dfff96daf6dcb83307abadd4
Marcin <bizior.marcin@googlemail.com>
2020-12-15 22:03:58 +0100

    db data base cereated

--- Modul_8/busines-card-web/app.py
+++ Modul_8/busines-card-web/app.py
@@ -1,17 +1,24 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
-
 from datetime import datetime
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///contacts.db'
+app.config['SECRET_KEY'] = "random string"
+
 db = SQLAlchemy(app)
 
-class contacts(db.Model):
+class Contacts(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(15), nullable=False)
     surname = db.Column(db.String(15), nullable=True)
-    message = db.Column(db.String(200),nullable=False)
+    message = db.Column(db.String(400),nullable=False)
     date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __init__(self, name, surname, message):
+        self.name = name
+        self.surname = surname
+        self.message = message
 
 @app.route('/')
 @app.route('/index.html')
@@ -22,33 +29,29 @@ def index():
 def about_me():
     return render_template("about_me.html")
 
+@app.route('/message_send.html')
+def done():
+    return render_template("message_send.html")
+
+
 @app.route('/contact_me.html', methods=['POST', 'GET'])
 def contact_me():
-    print("start def")
+
     if request.method == 'POST':
-        print("got posted")
-        content = (request.form['name'], request.form['surname'], request.form['message'])
-        new_message = content
-        print(content)
-        try:
+        if not request.form['name'] or not request.form['surname'] or not request.form['message']:
+            flash("Please enter all required data", 'error')
+        else:
+            new_message = Contacts(
+                request.form['name'], request.form['surname'], request.form['message'])
+
             db.session.add(new_message)
             db.session.commit()
-            return redirect('/')
-        except:
-            return "There was an issue with your form"
-        
-    else:
-        return render_template("contact_me.html")
+
+            flash("Message successfully send.")
+            return render_template("/message_send.html")
+    return render_template("/contact_me.html")
 
 
-
-
-
-
-
-
-
-
-
-if __name__=="__main__":    
-     app.run(debug=True)
+if __name__ == "__main__":
+    db.create_all()
+    app.run(debug=True)
--- Modul_9/waluty/app.py
+++ Modul_9/waluty/app.py
@@ -9,16 +9,19 @@ app = Flask(__name__)
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
+    print("index")
     if request.method == 'POST':
         currency = request.form['currency']
         amount = request.form['amount']
-        
+        print("POST - {currency}")
+        print("POST - {amount}")
 
         try:
-            print(calc())
+            pass
+            
 
         except:
-            return amount * exchange.value
+            return "it was an error :/"
 
     else:
         return render_template('corn_exchange.html')
