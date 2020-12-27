438e977a07aeff40f3617e427ec1f6652ee83698
Marcin <bizior.marcin@googlemail.com>
2020-12-28 00:23:50 +0100

    list update

--- Modul_9/home_music_library/app.py
+++ Modul_9/home_music_library/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, redirect, url_for
+from flask import Flask, request, render_template, redirect, url_for, json
 from forms import MusicLibrary
 from models import songs
 
@@ -23,47 +23,16 @@ def songs_list():
 def delete_song(song_id):
     form = MusicLibrary()
     error = ""
-    song = songs.delete(song_id -1)
+    song = songs.delete(song_id-1)
     if not song:
         abort(404)
         
     return render_template("index.html", form=form, songs=songs.all(), error=error)
 
 
-@app.route("/update/<int:song_id>", methods=["PUT"])
+@app.route("/index/<int:song_id>", methods=["PUT"])
 def update_song(song_id):
-    song = songs.get(song_id)
-    if not song:
-        abort(404)
-    if not request.json:
-        abort(400)
-    data = request.json
-    if any([
-        'title' in data and not isinstance(data.get('title'), str),
-        'band' in data and not isinstance(data.get('band'), str),
-        'genre' in data and not isinstance(data.get('genre'), str)
-    ]):
-        abort(400)
-    song = {
-        'title': data.get('title', song['title']),
-        'band': data.get('band', song['band']),
-        'genre': data.get('genre', song['genre'])
-    }
-    song.update(song_id, song)
-    return jsonify({'song': song})
-
-
-
-
-
-
-
-
-
-
-
-
-
+    pass
 
 
 @app.errorhandler(400)
