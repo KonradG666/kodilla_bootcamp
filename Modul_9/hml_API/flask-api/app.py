c632f978856464f829e9bcc6481e7b02119e337f
Marcin <bizior.marcin@googlemail.com>
2021-01-04 20:07:57 +0100

    update

--- Modul_9/hml_API/flask-api/app.py
+++ Modul_9/hml_API/flask-api/app.py
@@ -10,32 +10,36 @@ app.config["SECRET_KEY"] = "nininini"
 def songs_list_api():
     return jsonify(songs.all())
 
+
 @app.route("/api/v1/songs/<int:song_id>", methods=["GET"])
 def get_song(song_id):
     song = songs.get(song_id)
     if not song:
         abort(404)
     return jsonify({"song": song})
-    
+
+
 @app.route("/api/v1/songs/add/", methods=["POST"])
 def add_song():
-    if not request.json or not 'title' in request.json:
+    if not request.json or not "title" in request.json:
         abort(400)
     song = {
-        'id': songs.all()[-1]['id'] + 1,
-        'title': request.json.get('title'),
-        'band': request.json.get('band', ""),
-        'genre': request.json.get('genre', "")
+        "id": songs.all()[-1]["id"] + 1,
+        "title": request.json.get("title"),
+        "band": request.json.get("band", ""),
+        "genre": request.json.get("genre", ""),
     }
     songs.create(song)
-    return jsonify({'song': song}), 201
+    return jsonify({"song": song}), 201
+
 
 @app.route("/api/v1/songs/<int:song_id>", methods=["DELETE"])
 def delete_song(song_id):
     result = songs.delete(song_id)
     if not result:
-        abort(404)  
+        abort(404)
     return jsonify({"result": result})
+
 
 @app.route("/api/v1/songs/<int:song_id>", methods=["PUT"])
 def update_song(song_id):
@@ -45,28 +49,31 @@ def update_song(song_id):
     if not request.json:
         abort(400)
     data = request.json
-    if any([
-        'title' in data and not isinstance(data.get('title'), str),
-        'band' in data and not isinstance(data.get('band'), str),
-        'genre' in data and not isinstance(data.get('genre'), str)
-    ]):
+    if any(
+        [
+            "title" in data and not isinstance(data.get("title"), str),
+            "band" in data and not isinstance(data.get("band"), str),
+            "genre" in data and not isinstance(data.get("genre"), str),
+        ]
+    ):
         abort(400)
     song = {
-        'title': data.get('title', song['title']),
-        'band': data.get('band', song['band']),
-        'genre': data.get('genre', song['genre'])
+        "title": data.get("title", song["title"]),
+        "band": data.get("band", song["band"]),
+        "genre": data.get("genre", song["genre"]),
     }
     songs.update(song_id, song)
-    return jsonify({'song': song})
+    return jsonify({"song": song})
 
 
 @app.errorhandler(404)
 def not_found(error):
-    return make_response(jsonify({'error': 'Not found', 'status_code': 404}), 404)
+    return make_response(jsonify({"error": "Not found", "status_code": 404}), 404)
+
 
 @app.errorhandler(400)
 def bad_request(error):
-    return make_response(jsonify({'error': 'Bad request', 'status_code': 400}), 400)
+    return make_response(jsonify({"error": "Bad request", "status_code": 400}), 400)
 
 
 if __name__ == "__main__":
